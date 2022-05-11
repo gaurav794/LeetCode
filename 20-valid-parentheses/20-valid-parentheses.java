b74409e3a0f9de5a929fe6f4328addfcb594877a
Gaurav Sharma <77416997+gaurav794@users.noreply.github.com>
2022-05-10 22:27:53 -0400

    Time: 2 ms (80.94%), Space: 40 MB (97.50%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) 
+    {
+        //guard clause
+        if(s == null || s.length() == 0)
+        {
+            return false;
+        }
+        //Create Stack
+        Stack<Character> parentheses = new Stack<>();
+        
+        int pivot = 0;        
+        int strLength = s.length();
+        boolean condition = true;
+          
+        while(pivot < strLength)
+        {
+            char currentChar = s.charAt(pivot);    
+            switch(currentChar)
+            {
+                case '(','[','{' -> parentheses.push(currentChar);
+                case ')' -> {if(!parentheses.empty() && parentheses.peek() == '(') parentheses.pop();
+                             else condition = false;}
+                case ']' -> {if(!parentheses.empty() && parentheses.peek() == '[') parentheses.pop();
+                             else condition = false;}
+                case '}' -> {if(!parentheses.empty() && parentheses.peek() == '{') parentheses.pop();
+                             else condition = false;}
+                default -> pivot = pivot;
+                
+            }            
+            pivot++;
+        }
+        
+        
+        return parentheses.empty() && condition;
+        
+    }
+}
