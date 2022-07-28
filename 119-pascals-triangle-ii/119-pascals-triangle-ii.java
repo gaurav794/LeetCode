6a0929c4982534e5838dc158f0b92a40ae6fcbc3
Gaurav Sharma <77416997+gaurav794@users.noreply.github.com>
2022-07-28 14:35:48 -0400

    Time: 23 ms (5.11%), Space: 43 MB (6.17%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -1,29 +1,61 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) 
     {
-        List<List<Integer>> list = new ArrayList<>();
+//         List<List<Integer>> list = new ArrayList<>();
+//         int counter = 0;
         
-        for(int i=0;i <= rowIndex;i++)
-        {
-          List<Integer> row = new ArrayList<>();
-        // 1 
-          for(int j=0;j <= i;j++)
-            {
-                if(j == 0 || j == i) row.add(1);
-                else
-                  {
-                      //last row values of current index and previous index(index-1)
-                      System.out.println(list);
-                      int value = list.get(i-1).get(j) + list.get(i-1).get(j-1);
-                      System.out.println(value);
-                      row.add(value);
-                  }
+//         for(int i=0;i <= rowIndex;i++)
+//         {
+//           List<Integer> row = new ArrayList<>();
+//         // 1 
+//           for(int j=0;j <= i;j++)
+//             {
+//                System.out.println(""+ ++counter);
+              
+//                 if(j == 0 || j == i) row.add(1);
+//                 else
+//                   {
+//                       //last row values of current index and previous index(index-1)
+//                       int value = list.get(i-1).get(j) + list.get(i-1).get(j-1);
+//                       row.add(value);
+//                   }
             
-            }
+//             }
             
-            list.add(i,row);
+//             list.add(i,row);
     
+//         }
+//         return list.get(rowIndex);
+        
+        List<List<Integer>> result = new ArrayList<>();
+    int counter = 0;
+        for(int i = 0;i<=rowIndex;i++)
+        {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = new ArrayList<>();
+            int len = 0;
+            if(result.size() > 0)
+            {
+                len = result.size();
+                prevRow = result.get(i-1);
+            }
+                
+            row.add(1);
+            
+            if(len > 0)
+            {
+                for(int j=1;j<len;j++)
+                {
+                    System.out.println(++counter);
+                    row.add(prevRow.get(j-1)+prevRow.get(j));
+                }
+                row.add(1);
+            }
+
+            result.add(row);
         }
-        return list.get(rowIndex);
+
+        return result.get(rowIndex);
+        
     }
 }
