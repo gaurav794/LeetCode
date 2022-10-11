d5ef915765f411fae643e594603a10ef1a21692e
Gaurav Sharma <77416997+gaurav794@users.noreply.github.com>
2022-10-11 16:18:38 +0530

    Time: 8 ms (20.79%), Space: 57.4 MB (71.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -17,35 +17,36 @@ class Solution {
             return new int[0];
         }
         
-        int[] arr = new int[nums.length];
+        int[] result = new int[nums.length];
+        int[] leftPass = new int[nums.length];
+        int[] rightPass = new int[nums.length];
         int len = nums.length;
-        
-        // Product of values before 0 index
-        int runningProduct = 1;
-
-        // Left Pass
+                
+        // Left Pass operation which stores product of number from 0 to n-1 index
         // O(N)
+        // leftPass[0] = nums[0];
+        int prod = 1;
         for(int i=0;i<len;i++)
-        {            
-            arr[i] = runningProduct;
-            runningProduct = runningProduct*nums[i];
+        {  
+           prod*=nums[i];
+           leftPass[i] = prod;           
         }
         
-        //Reset
-        // Product of values after length-1 index
-        runningProduct = 1;
-        
-        // Right Pass
+        // Right Pass operation which stores product of number from n-1 to 0 index
         //O(N)
+        prod = 1;
         for(int i=len-1;i>=0;i--)
-        {            
-            //left Pass value * running Product
-            arr[i] = arr[i]*runningProduct;
-            runningProduct = runningProduct*nums[i];
+        {
+            prod*=nums[i];
+            rightPass[i] = prod;
         }
-        
-        
-        return arr;
+        result[0]=rightPass[1];
+        for(int i=1;i<len-1;i++)
+        {
+            result[i] = leftPass[i-1]*rightPass[i+1];
+        }  
+        result[len-1]=leftPass[len-2];
+        return result;
     }
     
        
